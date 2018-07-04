6288d49860ebd3ba7f7f25d945bf9b3bbccd2a4d
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-04 13:42:24 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
   def valid?
     if @length1 <=0 || @length2 <=0 || @length3 <=0
       false 
-    elsif @length1+@length2 < @length3 ||@length2+@length3 < @length1 || @length1+@length3 < @length2
+    elsif @length1+@length2 <= @length3 ||@length2+@length3 <= @length1 || @length1+@length3 <= @length2
       false
     else
       true
